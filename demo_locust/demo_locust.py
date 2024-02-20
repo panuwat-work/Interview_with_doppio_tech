9d1359595360151238e691a78cb6da353e6726ca
panuwat-work <159217676+panuwat-work@users.noreply.github.com>
2024-02-20 20:25:14 +0700

    Update demo_locust.py

--- demo_locust/demo_locust.py
+++ demo_locust/demo_locust.py
@@ -4,18 +4,39 @@ from locust import HttpUser, task, between
 
 class MyUser1(HttpUser):
     wait_time = between(1, 2)
-    host = 'https://petstore.swagger.io/#/pet'
+    host = 'https://reqres.in'
 
     @task
-    def get_api1(self):
-        self.get_api1_endpoint()
+    def call_api(self):
+        self.get_api_endpoint() #get api endpoint
+        self.create_use_api()   #post api endpoint
+        self.update_user()  #put api endpoint
+        self.patch_user()   #patch api endpoint
+        self.delete_user()  #delete api endpoint
 
-    def get_api1_endpoint(self):
-        self.client.get('/findPetsByStatus', name='pet (get)')
-        self.client.get('/findPetsByStatus', name='findPetsByStatus (get)')
-        self.client.get('/store/placeOrder', name='placeOrder (get)')
-        self.client.get('/store/getInventory', name='getInventory (get)')
-        self.client.get('/user/createUsersWithArrayInput', name='createUsersWithArray (get)')
+    def get_api_endpoint(self):
+        self.client.get('/api/users?page=2', name='LIST USERS (get)')
 
+    def create_use_api(self):
+        payload = {
+            "name": "morpheus",
+            "job": "leader"
+        }
+        self.client.post('/api/users', json=payload, name='CREATE USER (post)')
 
-#locust -f locust_demo.py
+    def update_user(self):
+        payload = {
+            "name": "morpheus",
+            "job": "zion resident"
+        }
+        self.client.put('/api/users/2', json=payload, name='UPDATE USER (put)')
+
+    def patch_user(self):
+        payload = {
+            "name": "morpheus",
+            "job": "zion resident"
+        }
+        self.client.patch('/api/users/2', json=payload, name='PATCH USER (patch)')
+
+    def delete_user(self):
+        self.client.delete('/api/users/2', name='DELETE USER (delete)')
